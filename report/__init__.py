07819cd6edf72a0dc33ebd4603821739d4c46ec6
Oleh Kharkevych <oleg.kharkevich@gmail.com>
2022-08-08 20:22:12 +0300

    Cleared __init__.py as imports in it do not provide the expected result, and there's no need to investigate it further at this stage.

--- report/__init__.py
+++
@@ -1,8 +0,0 @@
-# For now "canonical package imports" don't work in execute_report.py
-# Leaving this as it is to maybe be fixed later.
-
-# from .answer import Answer
-# from .question import Question
-# from .report import Report
-# from .survey import Survey
-# from .text import Text
